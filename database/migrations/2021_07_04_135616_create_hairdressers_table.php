f47346675bc785a6c0778c2d50d9c765398a2847
Kazuki Aoyama <mtblue.sky.0124@gmail.com>
2021-07-06 00:50:55 +0900

    初コミット

---
+++ database/migrations/2021_07_04_135616_create_hairdressers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHairdressersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hairdressers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hair_title');
+            $table->string('hair_talk');
+            $table->string('hair_movie');
+            $table->string('hair_img');
+            $table->datetime('arrivedate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hairdressers');
+    }
+}
