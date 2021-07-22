355b04692223cb130bb27466219ac563fc645148
Kazuki Aoyama <mtblue.sky.0124@gmail.com>
2021-07-22 12:14:33 +0900

    アプリ説明追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <p class="text-center">AnywHairは、いつでもあなたの髪型の好みを提示できる美容管理サービスです。</p>
+
             <div class="card">
                 <div class="card-header">{{ __('ログイン') }}</div>
 
